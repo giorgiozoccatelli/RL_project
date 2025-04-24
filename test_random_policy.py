8723d031369b506d808932fcb9789c53935a0e39
Romain Salvi <salvir.main@gmail.com>
2025-04-24 16:01:07 +0200

    Update test_random_policy.py

--- test_random_policy.py
+++ test_random_policy.py
@@ -1,37 +1,61 @@
+"""Test a random policy on the OpenAI Gym Hopper environment.
+
+    TASK 1: Play around with this code to get familiar with the
+            Hopper environment.
+
+            For example:
+                - What is the state space in the Hopper environment? Is it discrete or continuous?
+                - What is the action space in the Hopper environment? Is it discrete or continuous?
+                - What is the mass value of each link of the Hopper environment, in the source and target variants respectively?
+                - what happens if you don't reset the environment even after the episode is over?
+                - When exactly is the episode over?
+                - What is an action here?
+"""
+import pdb
 import gym
-from env.custom_hopper import *
 import numpy as np
+from env.custom_hopper import *
 
 def main():
-    env = gym.make("CustomHopper-source-v0")
+    env = gym.make('CustomHopper-source-v0')
+    # env = gym.make('CustomHopper-target-v0')
+
     print('State space:', env.observation_space)
     print('Action space:', env.action_space)
     print('Dynamics parameters:', env.get_parameters())
 
+    final_state = None
     n_episodes = 500
     render = True
-    final_qpos = None
-    final_qvel = None
 
     for episode in range(n_episodes):
-        if final_qpos is None:
-            env.reset()
+        if final_state is None:
+            state = env.reset()
         else:
             env.reset()
             env.set_state(final_qpos, final_qvel)
+            state = env.step(env.action_space.sample())[0]  # step to generate a new observation
+
+        print(f"\n=== Episode {episode} START ===")
+        print("Initial state:", state)
 
         done = False
+        total_reward = 0.0
+
         while not done:
             action = env.action_space.sample()
             state, reward, done, info = env.step(action)
+            total_reward += reward
+
             if render:
                 env.render()
 
-        # Save simulator state (qpos and qvel)
         final_qpos = env.sim.data.qpos.copy()
         final_qvel = env.sim.data.qvel.copy()
 
-        print(f"Episode {episode} ended.")
+        print("Final state:", state)
+        print("Total reward:", total_reward)
+        print(f"=== Episode {episode} END ===")
 
 if __name__ == '__main__':
     main()
